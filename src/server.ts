ea2fd0d72ddfa8ec6a204f64dd25ac0d3b305050
Ehsanul Bari Fahim <ehsanulbarifahim@localhost.localdomain>
2023-06-14 02:42:33 +0600

    winston logger setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/logger'
 
 async function University() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('successfully database connected')
+    logger.info('successfully database connected')
     app.listen(config.port, () => {
-      console.log(` Application listening on port ${config.port}`)
+      logger.info(` Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log('failed to connect database', err)
+    logger.error('failed to connect database', err)
   }
 }
 
